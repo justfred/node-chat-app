3d9ee61b5586560bd485c748e0b2de6dd9f33664
Frederick Heald <fheald@bandwidthx.com>
2018-02-22 10:48:39 -0800

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -17,11 +17,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newMessage', {
-  //   from: 'mike@example.com',
-  //   text: 'Wazzup',
-  //   createdAt: 123
-  // });
+  socket.emit('newMessage', {
+    from: 'admin',
+    text: 'welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'new user joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
@@ -30,6 +36,11 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () => {
