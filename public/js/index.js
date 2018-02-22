fc31de383fb04fda903744fb82540c135063b38b
Frederick Heald <fheald@bandwidthx.com>
2018-02-22 15:26:31 -0800

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -52,12 +52,16 @@ locationButton.on('click', function (e) {
     return alert('Geolocation not supported by your browser');
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function (position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   }, function (error)  {
     alert ('Unable to fetch location.');
+    locationButton.removeAttr('disabled').text('Send location');
   });
 });
