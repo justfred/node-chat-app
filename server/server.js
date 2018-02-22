f03c30162182af983b758e8286e9920d89cb348d
Frederick Heald <fheald@bandwidthx.com>
2018-02-22 10:38:00 -0800

    Emit new message on createMessage

--- server/server.js
+++ server/server.js
@@ -17,24 +17,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  // socket.emit('newEmail', {
+  // socket.emit('newMessage', {
   //   from: 'mike@example.com',
   //   text: 'Wazzup',
-  //   createAt: 123
-  // });
-
-  socket.emit('newMessage', {
-    from: 'mike@example.com',
-    text: 'Wazzup',
-    createdAt: 123
-  });
-
-  // socket.on('createEmail', (newEmail) => {
-  //   console.log('createEmail', newEmail);
+  //   createdAt: 123
   // });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
