a79f98aac71aa26efba4c7ebf164ecb886f34531
Frederick Heald <fheald@bandwidthx.com>
2018-02-23 14:23:22 -0800

    Mustache templates

--- public/js/index.js
+++ public/js/index.js
@@ -14,24 +14,27 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function(message) {
-  // console.log('New message', message);
   var formattedTime = moment(message.createdAt).format('kk:mm:ss');
-  var li = jQuery('<li></li>');
-    li.text(`${formattedTime} ${message.from}: ${message.text}`);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(message) {
   var formattedTime = moment(message.createdAt).format('kk:mm:ss');
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>')
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    url: message.url,
+    createdAt: formattedTime
+  });
 
-  li.text(`${formattedTime} ${message.from}: `);
-  a.attr('href', message.url);
-  li.append(a);
-
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
